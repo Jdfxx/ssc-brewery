b3fc79b69c04fd5408dab1eca32b263c31896d70
Filip <fhagno@yahoo.com>
2025-04-12 15:12:09 +0200

    Add access to all for GET api methods

--- src/main/java/guru/sfg/brewery/config/WebSecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package guru.sfg.brewery.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,13 +14,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(authorizeRequests -> {
-                    authorizeRequests.antMatchers(
+                    authorizeRequests
+                            .antMatchers(
                             "/",
                             "/webjars/**",
                             "/login",
-                            "/tresources/**",
+                            "/resources/**",
                             "/beers/find"
-                    ).permitAll();
+                    )
+                            .permitAll()
+                            .antMatchers(HttpMethod.GET, "/api/v1/beer/**").permitAll();
                 })
                 .authorizeRequests().anyRequest().authenticated()
                 .and()
