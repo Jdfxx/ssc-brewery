890cfb1092bf214dc28d3ed78dc64cc922050ef9
Filip <fhagno@yahoo.com>
2025-04-16 12:09:04 +0200

    Add custom auth filter

---
+++ src/main/java/guru/sfg/brewery/security/RestHeaderAuthFilter.java
@@ -0,0 +1,91 @@
+package guru.sfg.brewery.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.event.InteractiveAuthenticationSuccessEvent;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+public class RestHeaderAuthFilter extends AbstractAuthenticationProcessingFilter {
+    public RestHeaderAuthFilter(RequestMatcher requiresAuthenticationRequestMatcher) {
+        super(requiresAuthenticationRequestMatcher);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws AuthenticationException, IOException, ServletException {
+
+        String userName = getUsername(httpServletRequest);
+        String secret = getSecret(httpServletRequest);
+
+        if (userName == null || secret == null) {
+            userName = "";
+            secret = "";
+        }
+
+        log.debug("Attempting to authenticate user: " + userName);
+        log.debug("Attempting to authenticate secret: " + secret);
+
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(userName, secret);
+
+        if (!StringUtils.isEmpty(userName)) {
+            return this.getAuthenticationManager().authenticate(token);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+        if (!this.requiresAuthentication(request, response)) {
+            chain.doFilter(request, response);
+        } else {
+            if (this.logger.isDebugEnabled()) {
+                this.logger.debug("Request is to process authentication");
+            }
+
+            try {
+                Authentication authResult = attemptAuthentication(request, response);
+                if (authResult != null) {
+                    successfulAuthentication(request, response, chain, authResult);
+                } else {
+                    chain.doFilter(request, response);
+                }
+            } catch (AuthenticationException e) {
+                unsuccessfulAuthentication(request, response, e);
+            }
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        if (this.logger.isDebugEnabled()) {
+            this.logger.debug("Authentication success. Updating SecurityContextHolder to contain: " + authResult);
+        }
+        SecurityContextHolder.getContext().setAuthentication(authResult);
+
+    }
+
+    private String getSecret(HttpServletRequest httpServletRequest) {
+        return httpServletRequest.getHeader("Api-Secret");
+    }
+
+    private String getUsername(HttpServletRequest httpServletRequest) {
+        return httpServletRequest.getHeader("Api-Key");
+    }
+}
--- src/main/java/guru/sfg/brewery/security/WebSecurityConfig.java
+++ src/main/java/guru/sfg/brewery/security/WebSecurityConfig.java
@@ -1,8 +1,9 @@
-package guru.sfg.brewery.config;
+package guru.sfg.brewery.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,16 +11,29 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AndRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    public RestHeaderAuthFilter restHeaderAuthFilter(AuthenticationManager authenticationManager) {
+        RestHeaderAuthFilter filter = new RestHeaderAuthFilter(new AntPathRequestMatcher("/api/**"));
+        filter.setAuthenticationManager(authenticationManager);
+        return filter;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
+        http.addFilterBefore(restHeaderAuthFilter(authenticationManager()),
+                UsernamePasswordAuthenticationFilter.class);
+
         http
                 .authorizeRequests(authorizeRequests -> {
                     authorizeRequests
