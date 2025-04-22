c59cfceb80098d675448813a241183fd0f32f6bc
Filip <fhagno@yahoo.com>
2025-04-22 17:10:44 +0200

    Add custom auth annotation

--- src/test/java/guru/sfg/brewery/web/controllers/IndexControllerTestIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/IndexControllerTestIT.java
@@ -5,6 +5,7 @@ import guru.sfg.brewery.repositories.BeerRepository;
 import guru.sfg.brewery.repositories.CustomerRepository;
 import guru.sfg.brewery.services.BeerService;
 import guru.sfg.brewery.services.BreweryService;
+import guru.sfg.brewery.web.controllers.api.BeerOrderController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -29,6 +30,9 @@ public class IndexControllerTestIT extends BaseIt {
 
     @MockBean
     BeerService beerService;
+
+    @MockBean
+    BeerOrderController beerOrderController;
 
     @Test
     void testGetIndexSlash() throws Exception{
